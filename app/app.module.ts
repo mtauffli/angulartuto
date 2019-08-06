8e650da9d7764c4b2ff27651504fe0d62bf49533
mtauffli <dark_skelet@hotmail.fr>
2019-08-06 17:20:11 +0200

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+import { AppareilService } from './services/appareil.service';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    AppareilService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/services/appareil.service.ts
@@ -0,0 +1,16 @@
+export class AppareilService {
+	appareils = [
+		{
+			name: 'Machine à laver',
+			status: 'allumé'
+		},
+		{
+			name: 'Télévision',
+			status: 'allumé'	
+		},
+		{
+			name: 'Ordinateur',
+			status: 'éteint'
+		}
+	];	
+}
